a2b0b7751ee56f7c2eb04d299cc49e41a3869d10
apurvh <apurv.hajare@gmail.com>
2025-08-16 20:49:34 +0530

    add basics

---
+++ init.lua
@@ -0,0 +1,44 @@
+-- Leader first, then basic options
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- Essentials: predictable editing, UI, and performance
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 500
+vim.opt.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
+
+-- Indentation defaults (we can adjust per-language later)
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+-- jj to exit insert mode
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
+vim.keymap.set("i", "jf", "<Esc>", { desc = "Exit insert mode" })
+
+-- keep cursor centered when jumping/searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Clear search highlight with Esc in normal mode (does nothing else)
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+
+-- Quality-of-life: save/quit bindings (optional, remove if you dislike)
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Save" })
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>",  { desc = "Quit" })
+
+-- Minimal autocmds: highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function() vim.highlight.on_yank({ timeout = 120 }) end,
+  desc = "Briefly highlight yanked text",
+})
